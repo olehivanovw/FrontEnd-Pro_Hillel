0b1329c0ddb652d470a5ab80b8b8fe47381b53e8
olehivanovw <olehivanovw@gmail.com>
2023-05-19 15:14:00 +0300

    Complete homework

---
+++ 30_lesson/react-lifting-state/src/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react"
+
+function Name() {
+  const [name, setName] = useState('')
+
+  return (
+    <div>
+      <label htmlFor="name">Name: </label>
+      <input
+        id="name"
+        value={name}
+        onChange={event => setName(event.target.value)} />
+    </div>
+  )
+}
+
+function FavoriteAnimal({animal, onAnimalChange}) {
+  return (
+    <div>
+      <label htmlFor="animal">Favorite Animal: </label>
+      <input
+        id="animal"
+        value={animal}
+        onChange={onAnimalChange}
+      />
+    </div>
+  )
+}
+
+function Display({animal}) {
+  return <div>{`Ваше любимое животное: ${animal}!`}</div>
+}
+
+function App() {
+  const [animal, setAnimal] = useState('')
+
+  return (
+    <form>
+      <Name />
+      <FavoriteAnimal animal={animal} onAnimalChange={event => setAnimal(event.target.value)}/>
+      <Display animal={animal} />
+    </form>
+  )
+}
+
+export default App
