cb571c135b8812bd929e877458aaf6b4186f6480
olehivanovw <olehivanovw@gmail.com>
2024-01-23 18:27:41 +0200

    Added redux toolkit

--- 32_lesson/todo-list-redux-thunk/src/store/index.js
+++ 32_lesson/todo-list-redux-thunk/src/store/index.js
@@ -1,11 +1,4 @@
-import { createStore, applyMiddleware } from "redux"
 import { rootReducer } from "./reducers"
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from '@redux-devtools/extension'
+import {configureStore} from '@reduxjs/toolkit'
 
-export const store = createStore(
-  rootReducer,
-  composeWithDevTools(
-    applyMiddleware(thunk)
-  )
-)
+export const store = configureStore({ reducer: rootReducer })
